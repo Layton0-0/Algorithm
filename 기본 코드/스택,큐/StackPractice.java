f1a22fb3fe672ca251c98a675f8208343ebedbc2
Yunjeong Choi / 최윤정 <yeshan0224@naver.com>
2022-01-21 18:02:42 +0900

    vsc 자바 연동

---
+++ 기본 코드/스택,큐/StackPractice.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class StackPractice {
+  public static void main(String[] args){
+    Stack<Integer> s = new Stack<>();
+    s.push(5);
+    s.push(6);
+    s.push(2);
+    s.pop();
+
+    while(!s.isEmpty()){
+      System.out.println(s.peek()+" ");
+      s.pop();
+    }
+  }
+}
