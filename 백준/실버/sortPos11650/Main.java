63799f7de51c889b1b0950aba16f913164314154
Yunjeong Choi / 최윤정 <92416563+Layton0-0@users.noreply.github.com>
2022-07-28 16:01:26 +0900

    0728 자바 수정

--- 백준/실버/sortPos11650/Main.java
+++ 백준/실버/sortPos11650/Main.java
@@ -1,13 +1,16 @@
-package sortPos11650;
+package 백준.실버.sortPos11650;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readline());
-        
+        int n = Integer.parseInt(br.readLine());
 
         br.close();
-        
+
     }
 }
--- 백준/실버/sortPos11651/Main.java
+++ 백준/실버/sortPos11651/Main.java
@@ -1,28 +1,28 @@
-package sortPos11651;
+package 백준.실버.sortPos11651;
 
 import java.util.*;
 import java.io.*;
-public class Main{
-    public static void main(String[] args) throws IOException{
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readline());
+        int n = Integer.valueOf(br.readLine());
         int[][] pos = new int[n][2];
         StringTokenizer st = null;
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readline());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             pos[i][0] = Integer.valueOf(st.nextToken());
             pos[i][1] = Integer.valueOf(st.nextToken());
         }
+
         Arrays.sort(pos, (e1, e2) -> {
-            if(e1[1] < e2[1]) return 1;
-            else if(el[1] > e2[1]) return -1;
-            else if(el[0] < el[0]) return 1;
-            else if(el[0] > el[0]) return -1;
-            else return 0;
+            if (e1[1] == e2[1])
+                return e1[0] - e2[0];
+            return e1[1] - e2[1];
         });
-        
-        for(int[] p: pos) {
-            System.out.println(p[0], p[1]);
+
+        for (int[] p : pos) {
+            System.out.println(p[0] + " " + p[1]);
         }
         br.close();
     }
